9f65af8d29771fad1ec0cbdc3737fdb33071d7fa
wubolieng80594 <1581672842@qq.com>
2025-11-02 22:43:19 +0800

    修订API调用（2） xxAPI

--- src/otherAPIs/xxAPItesting.py
+++ src/otherAPIs/xxAPItesting.py
@@ -1,12 +1,14 @@
 import requests
 
-url = "https://v2.xxapi.cn/api/disease?word=咳嗽变异性哮喘"
-
+url = "https://v2.xxapi.cn/api/disease"
+a=input("询问什么信息：")
+params={"word":a}
 payload = {}
 headers = {
-    'User-Agent': 'xiaoxiaoapi/1.0.0'
+    "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36 Edg/141.0.0.0',
+    "Context-type":"application/json"
 }
 
-response = requests.get(url, headers = headers, data = payload)
+response = requests.get(url, headers = headers, params=params)
 
 print(response.text)
